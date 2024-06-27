566cd7cbe8b29e211de9d641357839f75f5d1a99
nghiakma <lam47788@gmail.com>
2024-06-27 23:40:05 +0700

    Jobs

--- app/src/main/java/com/example/jobapp_u/home/activity/JobViewActivity.kt
+++ app/src/main/java/com/example/jobapp_u/home/activity/JobViewActivity.kt
@@ -1,21 +1,121 @@
 package com.example.jobapp_u.home.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
+import androidx.navigation.navArgs
+import coil.load
+import com.google.android.material.chip.Chip
 import com.example.jobapp_u.R
+import com.example.jobapp_u.databinding.ActivityJobViewBinding
+import com.example.jobapp_u.home.viewmodel.StudentJobViewModel
+import com.example.jobapp_u.util.LoadingDialog
+import com.example.jobapp_u.util.Status.SUCCESS
+import com.example.jobapp_u.util.Status.LOADING
+import com.example.jobapp_u.util.Status.ERROR
+import com.example.jobapp_u.util.createSalaryText
+import com.example.jobapp_u.util.showToast
+
+private const val TAG = "JobViewActivity"
 
 class JobViewActivity : AppCompatActivity() {
+    private var _binding: ActivityJobViewBinding? = null
+    private val binding get() = _binding!!
+    private val args by navArgs<JobViewActivityArgs>()
+    private val studentJobViewModel by viewModels<StudentJobViewModel>()
+    private val loadingDialog by lazy { LoadingDialog(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_job_view)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        _binding = ActivityJobViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupUI()
+        setupObserver()
+
+    }
+
+    private fun setupUI() {
+        studentJobViewModel.fetchJobMetrics(args.job.uid)
+        val job = args.job
+        binding.apply {
+            ivPopOut.setOnClickListener {
+                finish()
+            }
+            ivCompanyLogo.load(job.imageUrl)
+            tvRole.text = job.role
+            tvCompanyLocation.text =
+                getString(R.string.field_company_and_location, job.name, job.city)
+            tvJobDescription.text = job.description
+            tvResponsibility.text = job.responsibility
+            tvSalary.text = createSalaryText(job.salary, this@JobViewActivity)
+            job.skillSet.forEach { job ->
+                createSkillSetChip(job)
+            }
         }
+    }
+
+    private fun setupObserver() {
+        studentJobViewModel.jobMetric.observe(this){ metricStatus ->
+            when(metricStatus.status){
+                LOADING -> {
+                    loadingDialog.show()
+                }
+                SUCCESS -> {
+                    loadingDialog.dismiss()
+                    val (studentCount, hasJobApplied) = metricStatus.data!!
+                    binding.tvStudentCount.text = studentCount.toString()
+                    if (hasJobApplied){
+                        binding.btnApply.text = getString(R.string.field_job_applied)
+                        binding.btnApply.isEnabled = false
+                    } else {
+                        binding.btnApply.isEnabled = true
+                        binding.btnApply.setOnClickListener {
+                            studentJobViewModel.applyJob(args.job.uid)
+                        }
+                    }
+                }
+                ERROR -> {
+                    val message = metricStatus.message!!
+                    showToast(this, message)
+                    loadingDialog.dismiss()
+                }
+            }
+        }
+
+        studentJobViewModel.applicationStatus.observe(this){appliedState ->
+            when(appliedState.status){
+                LOADING -> {
+                    loadingDialog.show()
+                }
+                SUCCESS -> {
+                    val message = appliedState.data!!
+                    showToast(this, message)
+                    finish()
+                    loadingDialog.dismiss()
+                }
+                ERROR -> {
+                    loadingDialog.dismiss()
+                    val message = appliedState.message!!
+                    showToast(this, message)
+                }
+            }
+        }
+    }
+
+    private fun createSkillSetChip(job: String) {
+        val chip = Chip(this@JobViewActivity)
+        chip.text = job
+        chip.chipBackgroundColor =
+            ContextCompat.getColorStateList(this@JobViewActivity, R.color.chip_background_color)
+        chip.setTextColor(this@JobViewActivity.getColor(R.color.chip_text_color))
+        chip.chipCornerRadius = 8f
+        chip.textSize = 14.0f
+        binding.requiredSkillSetChipGroup.addView(chip)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
--- app/src/main/java/com/example/jobapp_u/home/viewmodel/StudentJobViewModel.kt
+++ app/src/main/java/com/example/jobapp_u/home/viewmodel/StudentJobViewModel.kt
@@ -1,4 +1,121 @@
 package com.example.jobapp_u.home.viewmodel
 
-class StudentJobViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.example.jobapp_u.model.JobApplication
+import com.example.jobapp_u.util.Constants.Companion.COLLECTION_PATH_COMPANY
+import com.example.jobapp_u.util.Constants.Companion.COLLECTION_PATH_STUDENT
+import com.example.jobapp_u.util.Resource
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+private const val TAG = "StudentJobViewModelTAG"
+
+class StudentJobViewModel : ViewModel() {
+
+    private val mRealtimeDb: DatabaseReference by lazy { FirebaseDatabase.getInstance().reference }
+    private val currentStudentId by lazy { FirebaseAuth.getInstance().currentUser?.uid }
+
+    private val _applicationStatus: MutableLiveData<Resource<String>> = MutableLiveData()
+    val applicationStatus: LiveData<Resource<String>> = _applicationStatus
+
+    private val _jobMetric: MutableLiveData<Resource<Pair<Int, Boolean>>> = MutableLiveData()
+    val jobMetric: LiveData<Resource<Pair<Int, Boolean>>> = _jobMetric
+
+    fun fetchJobMetrics(jobId: String) {
+        viewModelScope.launch(IO) {
+            try {
+                Log.d(TAG, "fetchJobMetrics called: ")
+                _jobMetric.postValue(Resource.loading())
+                val studentCountDeffered = async { getStudentCount(jobId) }
+                val hasJobAppliedDeffered = async { getJobStatus(jobId) }
+                val studentCount = studentCountDeffered.await()
+                val hasJobApplied = hasJobAppliedDeffered.await()
+                val jobMetric = Pair(studentCount, hasJobApplied)
+                Log.d(TAG, "JobMetric : $jobMetric")
+                _jobMetric.postValue(Resource.success(jobMetric))
+            } catch (error: Exception) {
+                val errorMessage = error.message ?: ""
+                _jobMetric.postValue(Resource.error(errorMessage))
+            }
+        }
+    }
+
+    private suspend fun getStudentCount(jobId: String): Int {
+        val appliedCountPath = "$COLLECTION_PATH_COMPANY/$jobId"
+        val appliedCountRef = mRealtimeDb.child(appliedCountPath)
+        val appliedCountDeffered = CompletableDeferred<Int>()
+        val appliedCountListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val appliedCount = snapshot.children.count()
+                Log.d(TAG, "AppliedCount : $appliedCount")
+                appliedCountDeffered.complete(appliedCount)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d(TAG, "onCancelled: ${error.message}")
+                appliedCountDeffered.completeExceptionally(error.toException())
+            }
+        }
+        appliedCountRef.addValueEventListener(appliedCountListener)
+        appliedCountDeffered.invokeOnCompletion {
+            appliedCountRef.removeEventListener(appliedCountListener)
+        }
+        return appliedCountDeffered.await()
+    }
+
+    private suspend fun getJobStatus(jobId: String): Boolean {
+        val jobStatusPath =
+            "$COLLECTION_PATH_STUDENT/$currentStudentId/$COLLECTION_PATH_COMPANY/$jobId"
+        val jobStatusRef = mRealtimeDb.child(jobStatusPath)
+        val jobStatusDeffered = CompletableDeferred<Boolean>()
+        val jobStatusListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    jobStatusDeffered.complete(true)
+                } else {
+                    jobStatusDeffered.complete(false)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d(TAG, "onCancelled: ${error.message}")
+                jobStatusDeffered.completeExceptionally(error.toException())
+            }
+        }
+        jobStatusRef.addValueEventListener(jobStatusListener)
+        jobStatusDeffered.invokeOnCompletion {
+            jobStatusRef.removeEventListener(jobStatusListener)
+        }
+        return jobStatusDeffered.await()
+    }
+
+    fun applyJob(jobId: String) {
+        viewModelScope.launch(IO) {
+            try {
+                _applicationStatus.postValue(Resource.loading())
+                val jobApplication = JobApplication(jobId = jobId, studentId = currentStudentId!!)
+                val jobApplicationPath = "$COLLECTION_PATH_COMPANY/$jobId/$currentStudentId"
+                val studentJobApplicationPath = "$COLLECTION_PATH_STUDENT/$currentStudentId/$COLLECTION_PATH_COMPANY/$jobId"
+
+                val jobApplicationRef = mRealtimeDb.child(jobApplicationPath)
+                jobApplicationRef.setValue(jobApplication).await()
+
+                val studentJobApplicationRef = mRealtimeDb.child(studentJobApplicationPath)
+                studentJobApplicationRef.setValue(jobApplication).await()
+                _applicationStatus.postValue(Resource.success("Job applied success."))
+            } catch (error: Exception) {
+                val errorMessage = error.message ?: ""
+                _applicationStatus.postValue(Resource.error(errorMessage))
+            }
+        }
+    }
 }
